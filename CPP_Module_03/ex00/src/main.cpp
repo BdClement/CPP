93845908854148932ca0489c0b0837211458f13b
Clement BERNARD <clbernar@made-f0Ar10s1.clusters.42paris.fr>
2023-12-22 20:14:24 +0100

    starting CPPModule03

---
+++ CPP_Module_03/ex00/src/main.cpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clbernar <clbernar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 19:28:18 by clbernar          #+#    #+#             */
+/*   Updated: 2023/12/22 20:13:44 by clbernar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main()
+{
+	// Initialisation
+	ClapTrap	Claptrap1("Uno");
+	ClapTrap	Claptrap2("Dos");
+	ClapTrap	Claptrap3(Claptrap1);
+	ClapTrap	Claptrap4;
+	Claptrap4 = Claptrap2;
+	std::cout << std::endl;
+
+	std::cout << Claptrap1 << std::endl;
+	std::cout << Claptrap2 << std::endl;
+	std::cout << Claptrap3 << std::endl;
+	std::cout << Claptrap4 << std::endl;
+	std::cout << std::endl;
+
+	// Actions
+	Claptrap1.attack("Dos");
+	Claptrap2.takeDamage(Claptrap1.getAttackDamage());
+	Claptrap4.attack("Uno");
+	Claptrap3.takeDamage(Claptrap4.getAttackDamage());
+	std::cout << std::endl;
+	Claptrap2.beRepaired(5);
+	Claptrap3.beRepaired(3);
+	std::cout << std::endl;
+	return (0);
+}
